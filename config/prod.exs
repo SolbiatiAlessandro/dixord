e9a2e512ed2ec70e76e5260475a1772cf6f47e9e
Alessandro Solbiati <lessandro@fb.com>
2020-05-02 15:29:35 +0100

    [heroku] missing :database key, 3

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ config :dixord, DixordWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
-config :database, DixordWeb.Repo,
+config :dixord, Dixord.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
