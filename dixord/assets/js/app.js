1f8766d22d54f0c0561711b16903aaf1ff5ed037
Alessandro Solbiati <lessandro@fb.com>
2020-05-02 12:31:53 +0100

    [tutorial] channels front end, shout not working

--- dixord/assets/js/app.js
+++ dixord/assets/js/app.js
@@ -14,4 +14,32 @@ import "phoenix_html"
 // Import local files
 //
 // Local files can be imported directly using relative paths, for example:
-// import socket from "./socket"
+import socket from "./socket"
+
+let channel = socket.channel("room:lobby", {})
+channel.on('shout', function (payload) {
+	console.log("receiving shout ")
+	let li = document.createElement("li");
+	let name = payload.name || 'guest';
+	li.innerHtml = '<b>' + name + '</b>';
+	ul.appendChild(li);
+});
+
+channel.join();
+
+let ul = document.getElementById('msg-list');
+let name = document.getElementById('name');
+let msg = document.getElementById('msg');
+
+msg.addEventListener('keypress', function (event) {
+	console.log("keypress")
+	if (msg.value.lenght > 0){
+		console.log("shouting ")
+		console.log(msg.value)
+		channel.push('shout', {
+			name: name.value,
+			message: msg.value
+		});
+		msg.value = '';
+	}
+});
