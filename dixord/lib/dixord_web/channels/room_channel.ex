1f784bd928efed2ae860c1af23da3f2db35d2c70
Alessandro Solbiati <lessandro@fb.com>
2020-05-02 13:23:36 +0100

    [tutorial] data layer working

--- dixord/lib/dixord_web/channels/room_channel.ex
+++ dixord/lib/dixord_web/channels/room_channel.ex
@@ -3,6 +3,7 @@ defmodule DixordWeb.RoomChannel do
 
   def join("room:lobby", payload, socket) do
     if authorized?(payload) do
+      send(self(), :after_join)
       {:ok, socket}
     else
       {:error, %{reason: "unauthorized"}}
@@ -18,8 +19,18 @@ defmodule DixordWeb.RoomChannel do
   # It is also common to receive messages from the client and
   # broadcast to everyone in the current topic (room:lobby).
   def handle_in("shout", payload, socket) do
+    Dixord.Message.changeset(%Dixord.Message{}, payload) |> Dixord.Repo.insert
     broadcast socket, "shout", payload
     {:noreply, socket}
+  end
+
+  def handle_info(:after_join, socket) do
+    Dixord.Message.get_messages()
+    |> Enum.each(fn msg -> push(socket, "shout", %{
+         name: msg.name,
+         message: msg.message,
+	}) end)
+	{:noreply, socket}
   end
 
   # Add authorization logic here as required.
