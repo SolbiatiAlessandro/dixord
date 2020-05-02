531d791b9b3cd91694c085ff07c0fa1aca8d5f2f
Alessandro Solbiati <lessandro@fb.com>
2020-05-02 15:34:56 +0100

    [heroku] app crashing

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,8 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :dixord, DixordWeb.Endpoint,
-  url: [scheme: "https", host: "dixord.herokuapp.com/", port: 443],
+  url: [scheme: "https", host: "dixord.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
