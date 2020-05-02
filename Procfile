8df76cc20932e93e44dcd06608a5f3e681558da0
Alessandro Solbiati <lessandro@fb.com>
2020-05-02 18:48:13 +0100

    [docs] change plug.static

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: MIX_ENV=prod mix ecto.migrate && mix deps.get && mix docs -o priv/static/ && mix phx.server
+web: MIX_ENV=prod mix ecto.migrate && mix docs -o priv/static/ && mix phx.server
--- lib/dixord_web/endpoint.ex
+++ lib/dixord_web/endpoint.ex
@@ -13,7 +13,6 @@ defmodule DixordWeb.Endpoint do
     at: "/",
     from: :dixord,
     gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
