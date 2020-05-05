bca9b6d834443364cbb9dd6d56b9b563f6f1e7b2
Alessandro Solbiati <lessandro@fb.com>
2020-05-05 20:52:30 +0100

    [test] added chat_live_view_test for mount and send_message

--- test/dixord_web/live/chat_live_view_test.exs
+++ test/dixord_web/live/chat_live_view_test.exs
@@ -9,12 +9,34 @@ defmodule DixordWeb.ChatLiveViewTest do
   """
   use DixordWeb.ConnCase
   require Phoenix.LiveViewTest
+  require Logger
 
+  @doc"""
+  checks that the view mounts correctly
+  """
   test "mount", %{conn: conn} do
     {:ok, view, html} = Phoenix.LiveViewTest.live(conn, "/")
     # assert that there is the string Message
     # from the composer e.g. "Message this chat.."
     assert html =~ "Message"
     assert html =~ "Dixord"
+  end
+
+  @doc"""
+  sends a message and check that it's rendered
+  """
+  test "send_message", %{conn: conn} do
+    {:ok, view, html} = Phoenix.LiveViewTest.live(conn, "/")
+    message_content = "test_message_219120958120481023"
+    assert Phoenix.LiveViewTest.render_submit(
+     view, 
+      :message, 
+      %{"message" => %{
+        message: message_content,
+        author_name: "test_user",
+        profile_picture_url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3AUser_font_awesome.svg&psig=AOvVaw2VtYiITLYlpIobsaw-gyxO&ust=1588794095006000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNCzlbC9nekCFQAAAAAdAAAAABAH"
+        }
+      }
+    ) =~ message_content
   end
 end
